c395632ea67f11acc7b9e23f46d5bfa2e4dfb175
tudorteisanu <teisanutudort@gmail.com>
2023-06-22 13:22:26 +0300

    Restyled ticket-card

--- src/app/shared/components/ticket-card/ticket-card.component.ts
+++ src/app/shared/components/ticket-card/ticket-card.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-ticket-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './ticket-card.component.html',
   styleUrls: ['./ticket-card.component.scss']
 })
